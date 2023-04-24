8d622b3b8da8c1b04f2706a061cb239e524a527a
KhaledAlabssi <khaled.absi5@gmail.com>
2023-04-24 15:37:58 +0200

    code from js intro session

---
+++ week5/jsIntro/main.js
@@ -0,0 +1,66 @@
+// === Variable creating and output methods:
+
+// let firstName;
+// firstName = "Khaled";
+// let first_name;
+// let firstname;
+// console.log(firstName,age, lastName, "bla bla", 5);
+// "Hi, I'm fn ln, I'm age old, gender and from nationality"
+// alert("hi, my name is " + firstName + " "+ lastName)
+
+// const brand = "Adidas"
+// const price = 39
+// const size = 44
+// const color = "blue"
+// const status = "new"
+// const result = "Sam bought a " + status + " " + brand + ". It is " + color + " and it cost " + price + " Euros." 
+// console.log(result);
+// const advanced = `Sam bought a ${status} ${brand}. It is ${color} and it cost ${price}
+//  Euros.`
+// console.log(advanced);
+//========================
+
+// === String methods:
+
+// const place = "School"
+// const text = "Daria and Supriya"
+// console.log("We are learning with " + text, place);
+// const shortText = text.slice(2, 15)
+// console.log(shortText);
+// console.log(text.toUpperCase());
+// console.log(x.split(" "));
+// console.log(x.indexOf('and'));
+// console.log(x.charAt(3));
+// console.log(x.trim());
+
+// ========================
+
+// === Numbers and Operatores
+
+// console.log(7 + 3);
+// console.log('7' + '3');
+// console.log(7 + '3');
+
+let a;
+let b = false
+let c = 11
+let d = 12
+
+// const add = a + b
+// const sub = a - b
+// const div = a / b
+// const mul = a * b
+// console.log(`Add: ${add} | sub: ${sub}| div: ${div}| mul: ${mul}`);
+// const exp = a ** b
+// console.log(exp);
+// const rem = a % b
+// console.log("reminder: ", rem);
+
+// Conditional operator
+
+const name = "Khaled"
+const g = 88
+const result = g >= 18
+const print = `My name is ${name}. My age is ${g} and my driving state is: ${result}`
+
+console.log(print);
