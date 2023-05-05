bc9fbfb4277bebbaf2a8332532fb17a1a66ba835
KhaledAlabssi <khaled.absi5@gmail.com>
2023-05-05 06:42:58 +0200

    add objects and loops code to week 6

---
+++ week6/objects_loops/main.js
@@ -0,0 +1,96 @@
+const myLaptop = {
+  name: "z101",
+  brand: "MS",
+  size: "14 Inch",
+  price: "200",
+  color: "silver",
+  os: "windows 98",
+  processor: "Intel I7",
+  RAM: "16gb",
+  Display: "LCD",
+};
+
+// const meArray = ["KHlaed", 37, false, ["js", "Python", "Java"]]
+
+// const me = {
+//     name: "Khaled",
+//     age: 37,
+//     superPower: false,
+//     languages: ["js", "Python", "Java"],
+//     printInfo: function () {
+//         return
+//     }
+// }
+
+// me.money = .99
+// console.log(me);
+// console.log(me.printInfo());
+
+// const favFood = ["Fried Chicken", "Shawaram", "Nuggets"]
+
+// function printing(params) {
+//     console.log("I like to eat", params);
+
+// }
+// favFood.forEach(printing)
+
+// let names = ["Daria", "Swathi", "Tusharas", "Hani"];
+
+// let namesLength = [];
+
+// for (let i = 0; i < names.length; i++) {
+
+// // console.log(names[i].length);
+// namesLength.push(names[i].length)
+  
+// }
+
+// console.log(namesLength);
+
+
+// console.log(names[0].length);
+// console.log(names[1]);
+// console.log(names[2]);
+// console.log(names[3]);
+
+// console.log("Klkjasdf fadskljadfs af".length);
+
+
+// let numbers4 = [3, 65, 33, 102]
+
+// let sum = 0
+
+
+// for(let i = 0; i < numbers4.length; i++) {
+
+
+
+
+
+//     console.log(numbers4[i]);
+//     sum = sum + numbers4[i] // 
+// }
+
+// console.log(sum);
+
+
+// create a var sum = 0
+
+// creat for loop that loops thro the number4 array 
+// and add the value of its items to sum
+
+// log the sum var after for loop body
+
+
+let a = [1, 2, 3, 4, 5]
+let b = ["k", "b", "g", "j", "bla", "blo", "sss"]
+
+for (let i = 0; i < a.length; i++) {
+
+    console.log(b[i]);
+
+
+}
+
+
+
